c0d9ab9fdc5cb904f8534a86342c5335073c086f
Christopher Sexton <cpsexton@gmail.com>
2019-08-10 20:27:38 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,7 +14,7 @@ def long_planeteer_calls(array)
   
 end
 
-def find_the_cheese(argument)
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.include?(cheese_types)
 end
