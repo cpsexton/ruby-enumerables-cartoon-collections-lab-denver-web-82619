e2aa62722480328d9ad71cc0c61201e616db59a7
Christopher Sexton <cpsexton@gmail.com>
2019-08-10 19:38:26 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,12 +1,12 @@
 def roll_call_dwarves(dwarves)
   dwarves.each_with_index{ |item, index| 
   index = index + 1
-  
+  p index.kind_of?
   }
 end
 
 
-puts "#{1 + number.to_i} is way better"
+#puts "#{1 + number.to_i} is way better"
 
 
 def summon_captain_planet(veggies)
