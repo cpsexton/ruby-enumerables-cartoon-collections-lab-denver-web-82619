a30ae9c89b7cdda7071c8b2b4d510e768bd4848a
Christopher Sexton <cpsexton@gmail.com>
2019-08-10 17:57:05 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,7 @@
 def roll_call_dwarves(dwarves)
   fullArray = []
   num = 1
-  while num < dwarves.length + 1 do
+  while num < dwarves.length do
     fullArray.push(num)
     fullArray.push(dwarves[0])
     num += 1
